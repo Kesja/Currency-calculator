4e9b3c7cdac568a2417b7be4971d31a75508cd84
Joanna Bach <joannakatarzynabach@gmail.com>
2022-04-11 09:28:21 +0200

    Add if statment to convertPLNToUSD

--- src/utils/__tests__/convertPLNToUSD.js
+++ src/utils/__tests__/convertPLNToUSD.js
@@ -1,4 +1,4 @@
-import { convertPLNToUSD } from './../convertPLNtoUSD';
+import { convertPLNToUSD } from '../convertPLNtoUSD';
 
 describe('ConvertPLNtoUSD', () => {
   it('should return proper value when good input', () => {
@@ -8,8 +8,22 @@ describe('ConvertPLNtoUSD', () => {
     expect(convertPLNToUSD(12)).toBe('$3.43');
   });
   it('should return NaN when input is text', () => {
-    expect(convertPLNToUSD('6')).toBeNan();
+    expect(convertPLNToUSD('6')).toBeNaN();
     expect(convertPLNToUSD('abc')).toBeNaN();
     expect(convertPLNToUSD('-543')).toBeNaN();
+  });
+  it('should return NaN when input is empty', () => {
+    expect(convertPLNToUSD()).toBeNaN();
+  });
+  it('should return "Error" when input is different than number and string', () => {
+    expect(convertPLNToUSD({})).toBe('Error');
+    expect(convertPLNToUSD([])).toBe('Error');
+    expect(convertPLNToUSD(null)).toBe('Error');
+    expect(convertPLNToUSD(function () { })).toBe('Error');
+  });
+  it('should return zero when input is lower than zero', () => {
+    expect(convertPLNToUSD(-1)).toBe('$0.00');
+    expect(convertPLNToUSD(-2)).toBe('$0.00');
+    expect(convertPLNToUSD(-56)).toBe('$0.00');
   });
 });
--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,9 +1,13 @@
 
 export const convertPLNToUSD = (PLN) => {
   
-  if (typeof(PLN) === 'string') {
+  if (typeof(PLN) === 'string' || PLN === undefined) {
     return NaN;
-  }
+  } else if (typeof(PLN) !== 'string' && typeof(PLN) !== 'number') {
+    return 'Error';
+  } else if (PLN <= 0) {
+    return '$0.00';
+  };
 
   const PLNtoUSD = PLN / 3.5;
 
